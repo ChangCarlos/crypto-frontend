f4b0c29e508361445ffee1c04470194e5c91e836
ChangCarlos <carloshchang93@gmail.com>
2025-04-09 13:36:44 -0300

    Update API endpoints in coinGecko service to include '/api' prefix

--- src/services/coinGecko.js
+++ src/services/coinGecko.js
@@ -3,16 +3,17 @@ import axios from "axios";
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const getMarketTrend = async () => {
-  const response = await axios.get(`${API_URL}/market-trend`);
+  const response = await axios.get(`${API_URL}/api/market-trend`);
   return response.data;
 };
 
 export const getMarketUpdate = async () => {
-  const response = await axios.get(`${API_URL}/market-update`);
+  const response = await axios.get(`${API_URL}/api/market-update`);
   return response.data;
 };
 
 export const getCryptoChart = async (coinId) => {
-  const response = await axios.get(`${API_URL}/chart/${coinId}`);
+  const response = await axios.get(`${API_URL}/api/chart/${coinId}`);
   return response.data;
 };
+
